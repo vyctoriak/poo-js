5d8320023306e1471a42fe2a3055039689be2f8c
Vyctoria <vyctoriak@gmail.com>
2021-01-23 00:43:55 -0300

    atributos privados

---
+++ index.js
@@ -0,0 +1,44 @@
+class Cliente{
+  nome;
+  cpf;
+}
+
+class ContaCorrente {
+  agencia;
+  #saldo = 0;
+
+  saque(valor) {
+      if(this.#saldo >= valor) {
+          this.#saldo -=  valor;
+      }
+  }
+
+  depositar(valor) {
+      if(valor>0) {
+          this.#saldo += valor;
+          console.log(`Seu saldo é de R$${this.#saldo}`);
+      }
+  }
+
+}
+
+const cliente1 = new Cliente();
+const cliente2 = new Cliente();
+
+cliente1.nome = "Ricardo";
+cliente1.cpf = 11122233309;
+
+cliente2.nome = "Alice";
+cliente2.cpf = 88822233309;
+
+const contaCorrenteRicardo = new ContaCorrente();
+contaCorrenteRicardo.agencia = 1001;
+contaCorrenteRicardo.depositar(100);
+contaCorrenteRicardo.depositar(100);
+contaCorrenteRicardo.depositar(100);
+contaCorrenteRicardo.depositar(100);
+
+contaCorrenteRicardo.saque(50);
+
+
+console.log(contaCorrenteRicardo);
